a7f178e47e570c1b90626462452159d3132b560b
IamLAM <ingeniero@luismonge.com.mx>
2018-07-21 15:21:48 -0700

    inputs incluidos para interactuar

--- ShinyApp.R
+++ ShinyApp.R
@@ -3,27 +3,17 @@ library(shiny)
 #interfaz usuario
 ui<-pageWithSidebar(
   
-  headerPanel ("KM Litros"),
-  sidebarPanel(),
+  headerPanel("KM Litros"),
+  sidebarPanel(
+    selectInput("v","Mediciones:",choices=c("cilindros"="cyl","tranmision"="am","motores"="gear")),
+    checkboxInput("outliers","Mostrar outliers",TRUE)
+    
+  ),
   mainPanel() 
-  
-  
   
 )
 
 #server
-
-server<-function (input,output){
-  
-  
-}
-
-library(shiny)
-
-ui <- fluidPage(
-  
-)
-
 server <- function(input, output, session) {
   
 }
