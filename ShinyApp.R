485452d7cbbda3795285355ae53c0557efac200c
IamLAM <ingeniero@luismonge.com.mx>
2018-07-21 18:01:00 -0700

    Se crea el data y la función reactiva

--- ShinyApp.R
+++ ShinyApp.R
@@ -1,4 +1,10 @@
 library(shiny)
+#tratamiento de datos
+
+mpgData<-mtcars
+mpgData$am<-factor(mpgData$am,labels=c("Automatico","Manual"))
+
+
 
 #interfaz usuario
 ui<-pageWithSidebar(
@@ -15,6 +21,25 @@ ui<-pageWithSidebar(
 
 #server
 server <- function(input, output, session) {
+  
+  #expresion reactiva generando dos variables de salida
+  formulaTexto<-reactive(
+    {paste("mpg ~",input$v)})
+  output$Texto<-renderText(
+    {formulaTexto()
+    
+    })
+  
+  #desplegamos
+  output$mpgPloy<-renderPlot({
+    boxplot(as.formula(formulaTexto),
+            data=mpgData,
+            outline = input$outliers,
+            col="#75AAADB",pch=19
+            )
+    
+  })
+  
   
 }
 
